97a4482edf6bbe1fc11fbcea23f822af68f0150b
i068edo <138637025+i068edo@users.noreply.github.com>
2023-07-23 19:51:58 +0900

    Add files via upload

--- a.php
+++ a.php
@@ -1,7 +1,7 @@
 #!/usr/bin/php
 <?php 
   foreach(file('/usr/share/dict/words') as $line){
-    if( strpos($line, ''xy) !== false ){
+    if( strpos($line, 'xy') !== false ){
       echo "$line";
   } 
 ?>
